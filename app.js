e5897e917ca023bfdac4837816d9695045e498b0
ronald1710 <ronald.azofeifa@yahoo.com>
2022-09-02 23:52:57 +0000

    code terminado

---
+++ app.js
@@ -0,0 +1,35 @@
+const sum = (a,b) => {
+    return a + b
+}
+console.log(sum(7,3))
+
+
+let oneEuroIs = {
+    "JPY": 127.9, 
+    "USD": 1.2, 
+    "GBP": 0.8, 
+}
+//fromEuroToDollar
+const fromEuroToDollar = function(valueInEuro){
+    let valueInDollar = valueInEuro * 1.2;
+    return valueInDollar;
+}
+let oneDollarIs = {
+    "JPY": 130,
+}
+//fromDollarToYen
+const fromDollarToYen = function(valueInDollar){
+    let valueInYen = valueInDollar * 130;
+    return valueInYen;
+}
+let oneYenIs ={
+    "GBP": 0.9,
+}
+//fromYenToPound
+const fromYenToPound = function(valueInYen) {
+    let valueInPound= valueInYen * 0.9;
+    return valueInPound;
+}
+
+
+module.exports = { sum, fromEuroToDollar, fromDollarToYen, fromYenToPound}
