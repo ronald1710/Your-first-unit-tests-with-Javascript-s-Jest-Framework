a198f509e8e17c332cf99fe509da037592a42857
ronald1710 <ronald.azofeifa@yahoo.com>
2022-09-03 01:48:19 +0000

    terminado3.0

--- test.js
+++ test.js
@@ -1,27 +1,15 @@
-const { sum } = require('./app.js');
+const { sum, fromYenToPound, fromEuroToDollar, fromDollarToYen } = require('./app.js');
 test('adds 14 + 9 to equal 23', () => {
 
     let total = sum(14, 9);
     expect(total).toBe(23);
 });
 test("One euro should be 1.206 dollars", function(){
-    // importo la funcion desde app.js
-    const { fromEuroToDollar } = require('./app.js')
-    
-    // hago mi comparacion (la prueba)
-    expect(fromEuroToDollar(3.5)).toBe(4.2); 
+      expect(fromEuroToDollar(3.5)).toBe(4.2); 
 });
 test("One dolar should be 130 yen", function(){
-    // importo la funcion desde app.js
-    const { fromDollarToYen } = require('./app.js')
-    
-    // hago mi comparacion (la prueba)
     expect(fromDollarToYen(2.5)).toBe(325); 
 });
 test("One yen should be 0.9 pound", function(){
-    // importo la funcion desde app.js
-    const { fromYenToPound } = require('./app.js')
-    
-    // hago mi comparacion (la prueba)
     expect(fromYenToPound(5.5)).toBe(4.95); 
 })
